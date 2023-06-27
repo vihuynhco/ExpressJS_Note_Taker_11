caa8925c92fd5a89efc9c2606f74f735110afc35
vihuynhco <vihuynhco@gmail.com>
2023-06-26 18:13:20 -0600

    restructured files

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -3,12 +3,10 @@ const router = require("express").Router();
 
 router.get("/notes", (req, res) => {
     res.sendFile(path.join(__dirname, "../public/notes.html"));
-    }
-);
+    });
 
 router.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, "../public/index.html"));
-    }
-);
+    });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -6,15 +6,12 @@ const htmlRoutes = require('./routes/htmlRoutes');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-
-
 // Middleware for parsing JSON and urlencoded form data
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 app.use ('/api', apiRoutes)
 app.use('/', htmlRoutes);
-
 
 //starts the server to begin listening
 app.listen(PORT, () => console.log(`App listening on port ${PORT}`));
